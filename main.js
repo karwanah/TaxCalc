4f0af9901e4b070f0339ec7b61a71e19ec45189d
karwanah <karwan2156@gmail.com>
2021-12-21 02:30:12 +0000

    new design

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+// TaxBrackets
+
+var brackets {
+    [0, 7112, 0];
+    [1, 10732, 23];
+    [2, 20322, 28,5]
+};
+
+
 function calctax(gross, tax){
     // Write code to check tax brackets and tax refund.
     
 }
+
